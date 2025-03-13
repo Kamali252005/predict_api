2aac78747182814f41ceca0a50cb5b04b78b82c5
Kamali <kamalibaskaran21@gmail.com>
2025-03-13 11:26:16 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
+@app.route('/predict', methods=['GET'])
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+def predict():
+name= request.args.get('name')
 
-@app.route('/about')
-def about():
-    return 'About'
+if name:
+    return jsonify({'error': 'No file uploaded'}), 400
+
+else:
+    return jsonify({'status': 'success', 'filename': file.filename})
+    if_name_==_main_':
+        app.run(debug=True)
